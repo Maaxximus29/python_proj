50e12ff1085ea2b3f7a9b626fc688d45ec39cedb
Pranav <pranavraipur03@gmail.com>
2026-05-24 17:07:07 +0530

    commit

--- main.py
+++ main.py
@@ -5,3 +5,9 @@ message = "sausage"
 print(message)
 newText = 'I told my friend, "Python is my favorite language!"'
 print(newText)
+first_name = "Pranav"
+last_name = "Mallik"
+full_name = first_name + " " + last_name
+print(full_name)
+message = "CHECK," + full_name.title() + " !"
+print(message)
