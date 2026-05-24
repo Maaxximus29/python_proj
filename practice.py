90fd30ceab1d0a71da7a818ed9848c1e25524397
Pranav <pranavraipur03@gmail.com>
2026-05-24 19:07:47 +0530

    commit

--- practice.py
+++ practice.py
@@ -1,2 +1,12 @@
 name = "Pranav"
 output = "Hello " + name +", " + "would you like to learn some python today?"
+print(output)
+new_name = "chutad lover"
+print(new_name.title())
+famous_name = "Mutthal"
+mutthalMessage = 'said "Mutthal king Azimutthal"'
+print(famous_name + " " + mutthalMessage)
+age = 23
+# cannot concatanate an integer with a string
+#print("Happy " + age + "rd Birthday!")
+
