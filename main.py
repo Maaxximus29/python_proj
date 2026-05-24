7384320e2990ba14a0341bc28772eb3e42a824b1
Pranav <pranavraipur03@gmail.com>
2026-05-24 16:12:19 +0530

    some basic codes

---
+++ main.py
@@ -0,0 +1,7 @@
+print("Hello World")
+message = "Hello python world"
+print(message)
+message = "sausage"
+print(message)
+newText = 'I told my friend, "Python is my favorite language!"'
+print(newText)
